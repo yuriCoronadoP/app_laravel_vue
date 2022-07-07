624ffc58c283ee28145a8ff4a76ffd0876d23ec2
yuriCoronado <mtra.yuricoronado@gmail.com>
2022-07-06 21:46:52 -0500

    configuracion de app.js para importar vue y vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+import vue from 'vue'
+window.Vue = vue
+
+import App from './components/App.vue'
+
+// importamos axios
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+// importamos y configuramos vue-router
+import VueRouter from 'vue-router'
+import {routes} from './routes'
+import Vue from 'vue'
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+})
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+})
